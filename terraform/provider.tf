65bd4d5adefaf35d60949cbd38278bc5c29c492e
Allegro <alex.makovka@gmail.com>
2025-06-27 22:41:42 +0300

    autocomplete ansible/~~/secrets.yml

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -25,5 +25,5 @@ provider "yandex" {
 provider "datadog" {
   api_key = var.datadog_api_key
   app_key = var.datadog_app_key
-  api_url = "https://api.datadoghq.eu"
+  api_url = var.datadog_api_url
 }
